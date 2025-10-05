b1e66c26c37ee38a3f00652eed7dc3955b1d5737
Bilgehan Akcan <78697606+bilgehanAkcan@users.noreply.github.com>
2025-10-05 20:02:08 +0200

    Update test_hookcaller.py

--- testing/test_hookcaller.py
+++ testing/test_hookcaller.py
@@ -4,6 +4,7 @@ from typing import Callable
 from typing import TypeVar
 
 import pytest
+import time
 
 from pluggy import HookimplMarker
 from pluggy import HookspecMarker
@@ -198,6 +199,7 @@ def test_adding_wrappers_ordering_tryfirst(hc: HookCaller, addmeth: AddMeth) -> 
     def he_method3():
         yield  # pragma: no cover
 
+    time.sleep(120)
     assert funcs(hc.get_hookimpls()) == [he_method2, he_method1, he_method3]
 
 
